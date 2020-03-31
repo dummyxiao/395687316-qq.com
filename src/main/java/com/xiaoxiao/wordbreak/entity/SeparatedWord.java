77c42b5cff0ce3d24095009ace7842f652acbad0
dummyxiao <395687316@qq.com>
2020-04-01 00:30:28 +0800

    wordbreak

---
+++ src/main/java/com/xiaoxiao/wordbreak/entity/SeparatedWord.java
@@ -0,0 +1,39 @@
+package com.xiaoxiao.wordbreak.entity;
+
+import java.util.List;
+
+/**
+ * @author xiaobenneng@hotmail.com
+ * @date 2020/3/31
+ */
+public class SeparatedWord extends Word {
+
+    private List<Word> children;
+    public SeparatedWord(String word,List<Word> children) {
+        super(word);
+        this.children = children;
+    }
+    public SeparatedWord(String word,int index ,List<Word> children) {
+        super(word,index);
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof SeparatedWord) {
+            SeparatedWord obj1 = (SeparatedWord) obj;
+            return obj1.getWord().equals(this.getWord());
+        }
+        return false;
+    }
+
+    public List<Word> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Word> children) {
+        this.children = children;
+    }
+}
---
+++ src/main/java/com/xiaoxiao/wordbreak/entity/Word.java
@@ -0,0 +1,45 @@
+package com.xiaoxiao.wordbreak.entity;
+
+
+/**
+ * @author xiaobenneng@hotmail.com
+ * @date 2020/3/31
+ */
+public class Word {
+    private int index;
+    private String word;
+    public Word(String word) {
+        this.word = word;
+    }
+    public Word(String word,int index) {
+        this.word = word;
+        this.index = index;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj instanceof Word) {
+            Word obj1 = (Word) obj;
+            return obj1.word.equals(this.word);
+        }
+        return false;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
